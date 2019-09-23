03cfd273bf1bcc6cd3a93d83f334f5cc6f529d45
Nick <nicolas.chiu@jobcloud.ch>
2019-09-23 16:26:38 +0200

    save work

--- src/Command/RegisterChangedSchemasCommand.php
+++ src/Command/RegisterChangedSchemasCommand.php
@@ -8,9 +8,12 @@ use \RecursiveIteratorIterator;
 use \RecursiveDirectoryIterator;
 use \FilesystemIterator;
 use \SplFileInfo;
+use \AvroSchema;
+use \AvroSchemaParseException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use function FlixTech\SchemaRegistryApi\Requests\allSubjectVersionsRequest;
 use function FlixTech\SchemaRegistryApi\Requests\checkSchemaCompatibilityAgainstVersionRequest;
 use function FlixTech\SchemaRegistryApi\Requests\singleSubjectVersionRequest;
 use const FlixTech\SchemaRegistryApi\Constants\VERSION_LATEST;
@@ -47,53 +50,87 @@ class RegisterChangedSchemasCommand extends AbstractSchemaCommand
             )
         );
 
+        $avroFiles = [];
+
         /** @var SplFileInfo $file */
         foreach ($iterator as $file) {
             if (Avro::FILE_EXTENSION !== $file->getExtension()) {
                 continue;
             }
 
-            $isRegistered = true;
-            $schemaName = $file->getBasename('.' . Avro::FILE_EXTENSION);
-            $localSchema = json_encode(json_decode(file_get_contents($file->getRealPath())));
-
-            try {
-                $latestSchema = $response = $this->client->send(
-                    singleSubjectVersionRequest($schemaName, VERSION_LATEST)
-                );
-            } catch (RequestException $e) {
-                if (404 !== $e->getCode()) {
-                    throw $e;
-                } else {
-                    $isRegistered = false;
-                }
-            }
-
-            if (true === $isRegistered && $latestSchema === $localSchema) {
-                $output->writeln(sprintf('Schema %s has been skipped (no change)', $schemaName));
-                continue;
-            }
-
-            if (true === $isRegistered) {
-                $response = $this->client->send(
-                    checkSchemaCompatibilityAgainstVersionRequest(
-                        $localSchema,
-                        $file->getBasename(),
-                        VERSION_LATEST
-                    )
-                );
-
-                if (false === $response['is_compatible']) {
-                    $output->writeln(sprintf('Schema %s has an incompatible change', $schemaName));
-                    return -1;
-                }
-            }
-
-            $this->registry->register($schemaName, $localSchema);
-
-            $output->writeln(sprintf('Successfully registered new version of schema %s', $schemaName));
+            $avroFiles[$file->getBasename('.' . Avro::FILE_EXTENSION)] = $file->getRealPath();
         }
 
+        $abortRegister = false;
+
+        while (false === $abortRegister) {
+            foreach ($avroFiles as $schemaName => $avroFile) {
+
+                $isRegistered = true;
+                $latestSchema = null;
+                $localSchema = json_encode(json_decode(file_get_contents($avroFile)));
+
+                try {
+                    $response = $this->client->send(
+                        allSubjectVersionsRequest(
+                            $schemaName
+                        )
+                    );
+
+                    $schemaVersions = $this->getJsonDataFromResponse($response);
+
+                    $lastKey = array_key_last($schemaVersions);
+                    $latestVersion = $schemaVersions[$lastKey];
+                } catch (RequestException $e) {
+                    if (404 !== $e->getCode()) {
+                        throw $e;
+                    } else {
+                        $isRegistered = false;
+                    }
+                }
+
+                if (true === $isRegistered) {
+                    $response = $this->client->send(
+                        singleSubjectVersionRequest($schemaName, $latestVersion)
+                    );
+                    $latestSchema = $this->getJsonDataFromResponse($response)['schema'];
+                }
+
+                if (true === $isRegistered && $latestSchema === $localSchema) {
+                    $output->writeln(sprintf('Schema %s has been skipped (no change)', $schemaName));
+                    continue;
+                }
+
+                if (true === $isRegistered) {
+                    $response = $this->client->send(
+                        checkSchemaCompatibilityAgainstVersionRequest(
+                            $localSchema,
+                            $schemaName,
+                            $latestVersion
+                        )
+                    );
+
+                    $result = $this->getJsonDataFromResponse($response);
+
+                    if (false === $result['is_compatible']) {
+                        $output->writeln(sprintf('Schema %s has an incompatible change', $schemaName));
+                        return -1;
+                    }
+                }
+
+                try {
+                    $schema = AvroSchema::parse($localSchema);
+                } catch (AvroSchemaParseException $e) {
+                    continue;
+                }
+
+                $this->registry->register($schemaName, AvroSchema::parse($localSchema));
+
+                $output->writeln(sprintf('Successfully registered new version of schema %s', $schemaName));
+            }
+
+
+        }
         return 0;
     }
 }
