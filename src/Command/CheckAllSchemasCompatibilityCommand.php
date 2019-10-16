68ad5e7faeca4d46d57320a7da5709d1f32f2f72
Milan Radovic <milan.radovic@jobcloud.ch>
2019-10-16 12:58:01 +0200

    Check All schemas for compatibility (missing tests for now)

---
+++ src/Command/CheckAllSchemasCompatibilityCommand.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Jobcloud\SchemaConsole\Command;
+
+use FilesystemIterator;
+use GuzzleHttp\Exception\RequestException;
+use Jobcloud\SchemaConsole\Helper\Avro;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Throwable;
+
+class CheckAllSchemasCompatibilityCommand extends AbstractSchemaCommand
+{
+
+    /**
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setName('kafka-schema-registry:check:compatibility:all')
+            ->setDescription('Checks for compatibility for all schemas in folder')
+            ->setHelp('Checks for compatibility for all schemas in folder')
+            ->addArgument('schemaDirectory', InputArgument::REQUIRED, 'Path to avro schema directory');
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     * @return integer
+     * @throws RequestException
+     */
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        /** @var string $directory */
+        $directory = $input->getArgument('schemaDirectory');
+        $avroFiles = $this->getAvroFiles($directory);
+
+        $io = new SymfonyStyle($input, $output);
+
+        $failed = [];
+
+        if(false === $this->checkSchemas($avroFiles, $failed))
+        {
+            $io->error('Following schemas are not compatible:');
+            $io->listing($failed);
+
+            return 1;
+        }
+
+        return 0;
+    }
+
+    /**
+     * @param string $directory
+     * @return array
+     */
+    protected function getAvroFiles(string $directory): array
+    {
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(
+                $directory,
+                FilesystemIterator::SKIP_DOTS
+            )
+        );
+
+        $files = [];
+
+        /** @var SplFileInfo $file */
+        foreach ($iterator as $file) {
+            if (Avro::FILE_EXTENSION !== $file->getExtension()) {
+                continue;
+            }
+
+            $files[$file->getBasename('.' . Avro::FILE_EXTENSION)] = $file->getRealPath();
+        }
+
+        return $files;
+    }
+
+    /**
+     * @param string $schemaName
+     * @param string $localSchema
+     * @param string $latestVersion
+     * @return boolean
+     */
+    protected function isLocalSchemaCompatible(
+        string $schemaName,
+        string $localSchema,
+        string $latestVersion
+    ): bool {
+        return $this->schemaRegistryApi->checkSchemaCompatibilityForVersion(
+            $localSchema,
+            $schemaName,
+            $latestVersion
+        );
+    }
+
+    /**
+     * @param string $schemaName
+     * @param string $localSchema
+     * @return boolean
+     */
+    protected function isAlreadyRegistered(
+        string $schemaName,
+        string $localSchema
+    ): bool {
+        $version = null;
+
+        try {
+            $version = $this->schemaRegistryApi->getVersionForSchema(
+                $schemaName,
+                $localSchema
+            );
+        } catch (Throwable $e) {
+        }
+
+        return null !== $version;
+    }
+
+    /**
+     * @param array $avroFiles
+     * @param array $failed
+     * @return bool
+     */
+    private function checkSchemas(array $avroFiles, array &$failed = []): bool
+    {
+        $failed = [];
+
+        foreach ($avroFiles as $schemaName => $avroFile) {
+            /** @var string $fileContents */
+            $fileContents = file_get_contents($avroFile);
+
+            /** @var array $jsonDecoded */
+            $jsonDecoded = json_decode($fileContents);
+
+            /** @var string $localSchema */
+            $localSchema = json_encode($jsonDecoded);
+
+            $latestVersion = $this->schemaRegistryApi->getLatestSchemaVersion($schemaName);
+
+            if (false === $this->isLocalSchemaCompatible($schemaName, $localSchema, $latestVersion)) {
+                $failed[] = $schemaName;
+            }
+        }
+
+        return 0 === count($failed);
+    }
+}
