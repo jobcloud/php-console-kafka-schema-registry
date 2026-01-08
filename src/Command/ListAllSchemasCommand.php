7e80affe4ced630a0c54b6141cbb4de927a0dcaf
ig0rN <igorn995@gmail.com>
2026-01-08 18:40:21 +0100

    feat(CDA-1711): Extend ListAllSchemasCommand

--- src/Command/ListAllSchemasCommand.php
+++ src/Command/ListAllSchemasCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Jobcloud\SchemaConsole\Command;
 
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -17,7 +18,13 @@ class ListAllSchemasCommand extends AbstractSchemaCommand
         $this
             ->setName('kafka-schema-registry:list')
             ->setDescription('List all schemas')
-            ->setHelp('List all schemas');
+            ->setHelp('List all schemas')
+            ->addArgument(
+                'includeDeleted',
+                InputArgument::OPTIONAL,
+                'Include deleted schemas (use 
+                \'true\' or \'false\')'
+            );
     }
 
     /**
@@ -27,7 +34,17 @@ class ListAllSchemasCommand extends AbstractSchemaCommand
      */
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $schemas = $this->schemaRegistryApi->getSubjects();
+        $includeDeletedArg = $input->getArgument('includeDeleted');
+
+        if ($includeDeletedArg !== null && $includeDeletedArg !== 'true' && $includeDeletedArg !== 'false') {
+            $message = '<error>Invalid value for \'deletedSchemas\' argument. Allowed values are \'true\' or \'false\'.</error>';
+
+            $output->writeln($message);
+
+            return 1;
+        }
+
+        $schemas = $this->schemaRegistryApi->getSubjects($includeDeletedArg === 'true');
 
         foreach ($schemas as $schema) {
             $output->writeln((string) $schema);
