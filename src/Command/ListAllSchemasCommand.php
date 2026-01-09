6c1e8348ec69b16ff92988576f3888ef581a3f9a
ig0rN <igorn995@gmail.com>
2026-01-09 15:59:49 +0100

    feat(CDA-1711): Extend ListAllSchemasCommand

--- src/Command/ListAllSchemasCommand.php
+++ src/Command/ListAllSchemasCommand.php
@@ -31,7 +31,6 @@ class ListAllSchemasCommand extends AbstractSchemaCommand
         $schemas = $this->schemaRegistryApi->getSubjects(
             null === $includeDeletedArg ? 'false' : (string) $includeDeletedArg
         );
-        var_dump(null === $includeDeletedArg ? 'false' : (string) $includeDeletedArg);
 
         foreach ($schemas as $schema) {
             $output->writeln((string) $schema);
