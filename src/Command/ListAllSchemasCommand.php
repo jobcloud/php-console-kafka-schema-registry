20da5acdcb91673f0f5718cb49d6a07ae9aac355
ig0rN <igorn995@gmail.com>
2026-01-13 11:39:08 +0100

    feat(CDA-1711): Extend ListAllSchemasCommand

--- src/Command/ListAllSchemasCommand.php
+++ src/Command/ListAllSchemasCommand.php
@@ -26,6 +26,7 @@ class ListAllSchemasCommand extends AbstractSchemaCommand
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
+        throw new \InvalidArgumentException('test');
         $includeDeletedArg = $input->getArgument('includeDeleted');
 
         if (null !== $includeDeletedArg && false === in_array($includeDeletedArg, ['true', 'false'], true)) {
